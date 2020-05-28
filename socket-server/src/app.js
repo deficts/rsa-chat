fad2a120318de34d49501d24bab8a9282660cbdd
deficts <diegoar7@live.com>
2020-05-27 19:57:33 -0500

    Modificando el servidor para soportar ids y mensajes con nicknames

--- socket-server/src/app.js
+++ socket-server/src/app.js
@@ -2,14 +2,25 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
+const publicNarray= [];
+
 io.on('connection', function(socket) {
   console.log("A user connected");
+  console.log(socket.id);
+  io.emit('send-id',socket.id);
 
-  socket.on('new-message', (message) => {
-    io.emit('new-message',message);
+  socket.on('new-message', (nickname,message,id) => {
+    console.log(id);
+    io.emit('new-message',{nickname,message,id});
   });
+
+  socket.on('new-user', (publicN)=>{
+    publicNarray.push(publicN);
+    io.emit('public-N',publicNarray);
+  })
 });
 
 server.listen(3000,()=>{
   console.log("Socket.io server is listening on port 3000");
 });
+
